e479de6bcb9269111b13b615d248202121540308
mt019 <54445647+mt019@users.noreply.github.com>
2026-04-07 19:38:47 +0800

    Fix import statement and add missing newline

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // 注意：Vite 引用組件通常不需要寫 .jsx 後綴
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
